70a4f1b6510ff1f3d80b75fd3cbf8df128a67d0c
Kwab Fordjour <kwabford@gmail.com>
2018-06-20 16:13:54 -0400

    Initial Commit

---
+++ koibot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  koibot
+//
+//  Created by Kwabena A. Fordjour Jr. on 2018-06-20.
+//  Copyright © 2018 Kwabena A. Fordjour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
